9b35c8a1a225faefb66aeb509a8b9e4865f49ad8
Yiyang Huang <oliviahyy1126@gmail.com>
2021-06-24 16:41:45 +1000

    complete 1a exercises

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
+    </div>
+  );
+};
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercise1={exercises1}
+        exercise2={exercises2}
+        exercise3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;

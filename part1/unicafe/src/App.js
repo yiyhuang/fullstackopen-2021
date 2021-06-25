d143702c511f28944d6eeaf054f918ba1ea30f36
Yiyang Huang <oliviahyy1126@gmail.com>
2021-06-25 17:54:39 +1000

    complete exercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Statictic = ({ text, value }) => {
+  return (
+    <div>
+      {text} {value}
+    </div>
+  );
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text={"good"} />
+      <Button handleClick={() => setNeutral(neutral + 1)} text={"neutral"} />
+      <Button handleClick={() => setBad(bad + 1)} text={"bad"} />
+
+      <h1>statistics</h1>
+      <Statictic text={"good"} value={good} />
+      <Statictic text={"neutral"} value={neutral} />
+      <Statictic text={"bad"} value={bad} />
+    </div>
+  );
+};
+
+export default App;

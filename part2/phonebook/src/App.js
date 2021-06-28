6b3b2a9468f60ef96fd5a5b44ea5e05743b738b6
Yiyang Huang <oliviahyy1126@gmail.com>
2021-06-28 13:44:51 +1000

    complete exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -8,13 +8,22 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  var includeName = function (name) {
+    const personWithName = persons.filter((person) => person.name === name);
+    return personWithName.length > 0;
+  };
+
   const addPerson = (event) => {
     event.preventDefault();
-    const personObject = {
-      name: newName,
-    };
 
-    setPersons(persons.concat(personObject));
+    if (includeName(newName)) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      const personObject = {
+        name: newName,
+      };
+      setPersons(persons.concat(personObject));
+    }
     setNewName("");
   };
 

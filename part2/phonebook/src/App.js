b11850922579b6b029a6d7889492b1a6389f8966
Yiyang Huang <oliviahyy1126@gmail.com>
2021-07-06 17:40:52 +1000

    complete exercise 2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,12 +4,14 @@ import personService from "./services/persons";
 import Persons from "./components/Persons";
 import PersonForm from "./components/PersonForm";
 import NameFilter from "./components/NameFilter";
+import Notification from "./components/Notification";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -17,48 +19,66 @@ const App = () => {
     });
   }, []);
 
-  const addPerson = (event) => {
+  const addPerson = () => {
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+
+    personService.create(personObject).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName("");
+      setNewNumber("");
+
+      setMessage(`Added ${returnedPerson.name}`);
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    });
+  };
+
+  const updateNumber = (person) => {
+    if (
+      window.confirm(
+        `${person.name} is already added to the phonebook, replace the old number with a new one?`
+      )
+    ) {
+      const updatedPerson = { ...person, number: newNumber };
+      personService
+        .update(updatedPerson.id, updatedPerson)
+        .then((returnedPerson) => {
+          setPersons(
+            persons.map((person) =>
+              person.id !== updatedPerson.id ? person : returnedPerson
+            )
+          );
+          setMessage(`Updated ${returnedPerson.name}'s number`);
+          setTimeout(() => {
+            setMessage(null);
+          }, 5000);
+        })
+        .catch((error) => {
+          alert(
+            `the person '${updatedPerson.name}' was already deleted from server`
+          );
+          setPersons(
+            persons.filter((person) => person.id !== updatedPerson.id)
+          );
+        });
+      setNewName("");
+      setNewNumber("");
+    }
+  };
+
+  const handleAddPersonForm = (event) => {
     event.preventDefault();
 
     const person = persons.find((person) => person.name === newName);
+    // if person already exists
     if (person) {
-      if (
-        window.confirm(
-          `${person.name} is already added to the phonebook, replace the old number with a new one?`
-        )
-      ) {
-        const updatedPerson = { ...person, number: newNumber };
-        personService
-          .update(updatedPerson.id, updatedPerson)
-          .then((returnedPerson) => {
-            setPersons(
-              persons.map((person) =>
-                person.id !== updatedPerson.id ? person : returnedPerson
-              )
-            );
-          })
-          .catch((error) => {
-            alert(
-              `the person '${updatedPerson.name}' was already deleted from server`
-            );
-            setPersons(
-              persons.filter((person) => person.id !== updatedPerson.id)
-            );
-          });
-        setNewName("");
-        setNewNumber("");
-      }
+      updateNumber(person);
     } else {
-      const personObject = {
-        name: newName,
-        number: newNumber,
-      };
-
-      personService.create(personObject).then((returnedPerson) => {
-        setPersons(persons.concat(personObject));
-        setNewName("");
-        setNewNumber("");
-      });
+      addPerson();
     }
   };
 
@@ -77,7 +97,12 @@ const App = () => {
   const handleDelete = (personId) => {
     const person = persons.find((person) => person.id === personId);
     if (window.confirm(`Delete ${person.name}?`)) {
-      personService.destroy(personId);
+      personService.destroy(personId).then(() => {
+        setMessage(`Deleted ${person.name}`);
+        setTimeout(() => {
+          setMessage(null);
+        }, 5000);
+      });
       setPersons(persons.filter((person) => person.id !== personId));
     }
   };
@@ -91,6 +116,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} />
       <NameFilter
         label="filter shown with "
         filter={filter}
@@ -99,7 +125,7 @@ const App = () => {
 
       <h2>Add a new</h2>
       <PersonForm
-        addPerson={addPerson}
+        addPerson={handleAddPersonForm}
         newName={newName}
         handleNameChange={handleNameChange}
         newNumber={newNumber}
